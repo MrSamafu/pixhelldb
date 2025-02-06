71a5181370d0e26dd0816a9146748b66fcdcf3fe
MrSamafu <samson.flamme@hotmail.fr>
2025-02-06 23:31:25 +0100

    Projet configuré

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import "../styles/main.scss";
 	let { children } = $props();
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html'  // Permet de gérer les routes dynamiques via le client
+			fallback: 'index.html',  // Permet de gérer les routes dynamiques via le client
 		  })
 	}
 };
